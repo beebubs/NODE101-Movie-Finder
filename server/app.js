ebe5f2c26355a497535df8baea501e13df7a2a08
Bernadette B <90285283+beebubs@users.noreply.github.com>
2022-04-11 15:44:18 -0700

    shows data from cache

--- server/app.js
+++ server/app.js
@@ -1,8 +1,55 @@
+//import express, morgan and axios using require
 const express = require('express');
+const morgan = require("morgan");
+const axios = require('axios').default;
 
+//allows me to use environmental variables
+require('dotenv').config();
 
+//set up express server
 const app = express();
 
-// When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
+let movieObj = {};
+
+// add your routes and middleware below
+app.use(morgan("dev"));
+
+
+app.get("/", function(req, res) {
+    console.log("req.url", req.url)
+
+    //if user does not enter a movie ID or title parameter then respond with directions on how to access movie data
+    if(req.url == "/"){
+        res.status(200).send("enter movie ID or title parameter after the / in the url");
+    }else{
+
+    axios({
+        method:"get",
+        // When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
+        url: `http://www.omdbapi.com${req.url}&apikey=${process.env.REACT_APP_API_KEY}`,
+    })
+        .then((result) => {
+            // respond with data from cache if the req.url already exists as a key within movieObj
+            
+            if(movieObj.hasOwnProperty(req.url)){
+                console.log("sending data from cache", movieObj[req.url])
+                console.log("movieObj in if", movieObj);
+                res.send(movieObj[req.url]);
+
+            //make API call if it is a new request
+            }else{
+            movieObj[req.url] = result.data;
+            console.log("sending api call")
+            console.log("movieObj in else", movieObj);
+            res.send(result.data);
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            res.send('An error occured.');
+        })
+    }
+    
+});
 
 module.exports = app;
